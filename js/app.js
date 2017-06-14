789462455a40a2404dc0778685f0fa89065b8aa0
leadd <ddeal812@gmail.com>
2017-06-13 23:10:05 -0500

    Add cat level

--- js/app.js
+++ js/app.js
@@ -4,6 +4,18 @@ var ViewModel = function() {
     this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
     this.imgAttribution = ko.observable('https://www.flickr.com/photos/big');
 
+    this.level = ko.computed(function() {
+        if (this.clickCount() <= 10) {
+            return 'Newborn'
+        } else if (this.clickCount() > 10 && this.clickCount() <= 25) {
+            return 'Infant';
+        } else if (this.clickCount() > 25 && this.clickCount() <= 40) {
+            return 'Teen';
+        } else if (this.clickCount() > 40) {
+            return 'Adult';
+        }
+    }, this);
+
     this.incrementCounter = function() {
         this.clickCount(this.clickCount() + 1);
     };
