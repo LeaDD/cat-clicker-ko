7b2c09230df5a26517aed4f6715a255580af3240
leadd <ddeal812@gmail.com>
2017-06-15 21:18:26 -0500

    Build functionality do display multiple cats

--- js/app.js
+++ js/app.js
@@ -1,8 +1,46 @@
-var Cat = function() {
-    this.clickCount = ko.observable(0);
-    this.name = ko.observable('Tabby');
-    this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
-    this.imgAttribution = ko.observable('https://www.flickr.com/photos/big');
+var initialCats = [
+    {
+        clickCount : 0,
+        name : 'Tabby',
+        imgSrc : 'img/434164568_fea0ad4013_z.jpg',
+        imgAttribution : 'https://www.flickr.com/photos/bigtallguy/434164568',
+        nicks: [{nick: 'tabtab'},{nick: 't-muffin'},{nick: 'tabariffic'},{nick:'Ponch'}]
+    },
+    {
+        clickCount : 0,
+        name : 'Tiger',
+        imgSrc : 'img/4154543904_6e2428c421_z.jpg',
+        imgAttribution : 'https://www.flickr.com/photos/xshamx/4154543904',
+        nicks: [{nick:'Tigger'}]
+    },
+    {
+        clickCount : 0,
+        name : 'Scaredy',
+        imgSrc : 'img/22252709_010df3379e_z.jpg',
+        imgAttribution : 'https://www.flickr.com/photos/kpjas/22252709',
+        nicks: [{nick:'Shooby'}]
+    },
+    {
+        clickCount : 0,
+        name : 'Shadow',
+        imgSrc : 'img/1413379559_412a540d29_z.jpg',
+        imgAttribution : 'https://www.flickr.com/photos/malfet/1413379559',
+        nicks: [{nick:'Casper'}]
+    },
+    {
+        clickCount : 0,
+        name : 'Sleepy',
+        imgSrc : 'img/9648464288_2516b35537_z.jpg',
+        imgAttribution : 'https://www.flickr.com/photos/onesharp/9648464288',
+        nicks: [{nick:'Zzzzz'}]
+    }
+];
+
+var Cat = function(data) {
+    this.clickCount = ko.observable(data.clickCount);
+    this.name = ko.observable(data.name);
+    this.imgSrc = ko.observable(data.imgSrc);
+    this.imgAttribution = ko.observable(data.imgAttribution);
 
     this.level = ko.computed(function() {
         if (this.clickCount() <= 10) {
@@ -16,25 +54,33 @@ var Cat = function() {
         }
     }, this);
 
-    this.nicks = ko.observableArray([
-        {nick: 'tabtab'},
-        {nick: 't-muffin'},
-        {nick: 'tabariffic'}
-    ]);
+    this.nicks = ko.observableArray(data.nicks);
 }
 
 var ViewModel = function() {
     //Forces the context to stay within the viewmodel
     var that = this;
 
-    this.currentCat = ko.observable(new Cat());
+    this.catList = ko.observableArray([]);
 
+    initialCats.forEach(function(catItem) {
+        that.catList.push(new Cat(catItem));
+    });
+
+    this.currentCat = ko.observable(this.catList()[0]);
+
+    this.setCat = function(clickedCat) {
+        //alert(index);
+        that.currentCat(clickedCat);
+    };
 
     this.incrementCounter = function() {
-        //The 'with' keyword in the DOM is placing this in the currentCat binding
+        //The 'with' keyword in the DOM is placing this call in the currentCat binding
         //context. Could have simply used 'this' here instead of 'that.currentCat'
-        //but the latter helps with maintainability in showing that the currentCat
-        //is being referenced.
+        //but the latter accesses the outer 'this' andhelps with maintainability in
+        //showing that the currentCat is being referenced. 'this.currentCat' produces an
+        //error because, as mentioned before, the 'with' keyword set the binding to currentCat
+        //so effectively 'this.currentCat' would have been 'currentCat.currentCat'
         that.currentCat().clickCount(that.currentCat().clickCount() + 1);
     };
 }
